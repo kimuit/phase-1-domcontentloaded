fa807df3227a313cc6052cb98af52d5b3e6586c4
kimuit117 <kemoikiuita@gmail.com>
2022-09-01 03:18:45 +0300

    complete assignment

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Your code goes here
+
+document.addEventListener('DOMContentLoaded', function() {
+    document.getElementById('text').textContent = "This is really cool!";
+})
+
+// document.addEventListener('DOMContentLoaded', (event) => {
+//     document.getElementById('text').innerHTML = 'This is really cool!';
+// });
